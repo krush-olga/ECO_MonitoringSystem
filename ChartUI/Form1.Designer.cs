64265bd630f8d1614c1e9acf4081aad2947712ce
krush-olga <39516124+krush-olga@users.noreply.github.com>
2021-09-28 20:48:44 +0300

    Add files via upload

---
+++ ChartUI/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace ChartUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(832, 580);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1049, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(369, 577);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(31, 33);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Bar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(31, 56);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Line";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(31, 102);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(61, 17);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "Column";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(31, 125);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(48, 17);
+            this.checkBox5.TabIndex = 7;
+            this.checkBox5.Text = "Area";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(31, 79);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(41, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Pie";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chart5);
+            this.panel1.Controls.Add(this.chart4);
+            this.panel1.Controls.Add(this.chart3);
+            this.panel1.Controls.Add(this.chart2);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Location = new System.Drawing.Point(159, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(866, 580);
+            this.panel1.TabIndex = 10;
+            // 
+            // chart5
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart5.Legends.Add(legend2);
+            this.chart5.Location = new System.Drawing.Point(0, 0);
+            this.chart5.Name = "chart5";
+            this.chart5.Size = new System.Drawing.Size(866, 580);
+            this.chart5.TabIndex = 3;
+            this.chart5.Text = "chart5";
+            this.chart5.Click += new System.EventHandler(this.chart5_Click);
+            // 
+            // chart4
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart4.Legends.Add(legend3);
+            this.chart4.Location = new System.Drawing.Point(25, 10);
+            this.chart4.Name = "chart4";
+            this.chart4.Size = new System.Drawing.Size(832, 577);
+            this.chart4.TabIndex = 2;
+            this.chart4.Text = "chart4";
+            // 
+            // chart3
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart3.Legends.Add(legend4);
+            this.chart3.Location = new System.Drawing.Point(17, 2);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(832, 577);
+            this.chart3.TabIndex = 1;
+            this.chart3.Text = "chart3";
+            // 
+            // chart2
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart2.Legends.Add(legend5);
+            this.chart2.Location = new System.Drawing.Point(3, 0);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(832, 577);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 635);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ ChartUI/Form1.cs
@@ -0,0 +1,214 @@
+﻿using Data;
+using DrawChartModule.Models;
+using DrawChartModule.QueryHandlers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ChartUI
+{
+    public partial class Form1 : Form
+    {
+        private readonly TypeOfCharts type;
+        private readonly string title;
+        private readonly SeriesDataInPoints<double>[] data;
+        private DBManager dbManager = new DBManager();
+        private List<Chart> listOfCharts = new List<Chart>();
+        SeriesDataInPoints<double>[] dataChart;
+        string titleChart;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void DrawChartUI(TypeOfCharts type, string title, Dictionary<string, CustomPoint<double>> infoForTable , params SeriesDataInPoints<double>[] data )
+        {
+            chart1.Visible = false;
+            chart2.Visible = false;
+            chart3.Visible = false;
+            chart4.Visible = false;
+            chart5.Visible = false;
+            this.chart1.ChartAreas[0].AxisX.Title = "Серії розрахунків";
+            this.chart1.ChartAreas[0].AxisY.Title = "Значення";
+            this.chart2.ChartAreas[0].AxisX.Title = "Серії розрахунків";
+            this.chart2.ChartAreas[0].AxisY.Title = "Значення"; 
+            this.chart3.ChartAreas[0].AxisX.Title = "Серії розрахунків";
+            this.chart3.ChartAreas[0].AxisY.Title = "Значення"; 
+            this.chart4.ChartAreas[0].AxisX.Title = "Серії розрахунків"; 
+            this.chart4.ChartAreas[0].AxisY.Title = "Значення";
+            this.chart5.ChartAreas[0].AxisX.Title = "Серії розрахунків"; 
+            this.chart5.ChartAreas[0].AxisY.Title = "Значення"; 
+
+            dataChart = data;
+            titleChart = title;
+            DrawChart<double>.Draw(ref chart1, type, title, data);
+            DrawChart<double>.Draw(ref chart2, TypeOfCharts.Bar, title, data);
+            DrawChart<double>.Draw(ref chart3, TypeOfCharts.Pie, title, data);
+            DrawChart<double>.Draw(ref chart4, TypeOfCharts.Column, title, data);
+            DrawChart<double>.Draw(ref chart5, TypeOfCharts.Area, title, data);
+            dataGridView1.Columns.Add("first", "Серії розрахунків");
+            dataGridView1.Columns.Add("second", "Значення");
+            dataGridView1.Columns.Add("Info", "Інформація");
+            List<DataGridViewRow> list = new List<DataGridViewRow>();
+            foreach (var item in infoForTable)
+            {
+                dataGridView1.Rows.Add(item.Value.X, item.Value.Y, item.Key);
+            }
+
+            if (type == TypeOfCharts.Bar)  
+                checkBox1.Checked = true;          
+            if (type == TypeOfCharts.Pie)
+                checkBox3.Checked = true;
+            if (type == TypeOfCharts.Column)
+                checkBox4.Checked = true;
+            if (type == TypeOfCharts.Area)
+                checkBox5.Checked = true;
+            if (type == TypeOfCharts.Line)
+            {
+                chart1.Visible = true;
+                checkBox2.Checked = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                chart1.Visible = false;
+                chart3.Visible = false;
+                chart4.Visible = false;
+                chart5.Visible = false;
+                chart2.Visible = true;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox2.Checked)
+            {
+                chart1.Visible = true;
+                chart2.Visible = false;
+                chart3.Visible = false;
+                chart4.Visible = false;
+                chart5.Visible = false;
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+            }
+            else
+            {
+          
+            }
+        }
+
+        private void checkBox3_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+            if (checkBox3.Checked)
+            {
+                chart1.Visible = false;
+                chart3.Visible = true;
+                chart4.Visible = false;
+                chart5.Visible = false;
+                chart2.Visible = false;
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox4.Checked = false;
+                checkBox5.Checked = false;
+            }
+            else
+            {
+               
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox4.Checked)
+            {
+                chart1.Visible = false;
+                chart3.Visible = false;
+                chart4.Visible = true;
+                chart5.Visible = false;
+                chart2.Visible = false;
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox5.Checked = false;
+            }
+            else
+            {
+             
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox5.Checked)
+            {
+                chart1.Visible = false;
+                chart3.Visible = false;
+                chart4.Visible = false;
+                chart5.Visible = true;
+                chart2.Visible = false;
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            }
+            else
+            {
+            
+            }
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
